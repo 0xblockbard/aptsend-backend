00c08d824dab8a0cc4615952ad7ce0d0f4e41bf1
0xblockbard <>
2025-10-07 00:24:28 +0700

    Add horizon login

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
     return response()->json([
         'message' => 'API is running',
         'status' => 'ok'
     ]);
 });
+
+Route::get('/login/{user}', function ($user) {
+    if (! request()->hasValidSignature()) {
+        abort(401);
+    }
+    
+    $user = User::findOrFail($user);
+    Auth::login($user);
+    
+    return redirect('/horizon');
+    
+})->name('login.auto');
